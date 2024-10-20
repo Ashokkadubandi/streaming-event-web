2ded0d4e057f1c5ac2bc3b2e3190c016766943c9
Ashok <velmaashok765@gmail.com>
2024-10-20 06:36:28 +0000

    upcoming list modified

--- src/components/RecommendCard/index.js
+++ src/components/RecommendCard/index.js
@@ -3,15 +3,22 @@ import "./index.css";
 
 const RecShows = (props) => {
   const { events } = props;
-  const { date, imgUrl, eventName, distanceKm } = events;
+  const { date, imgUrl, eventName, cityName, distanceKm, weather } = events;
   let slicer = imgUrl.slice(32, 65);
   const imageUrl = `${process.env.REACT_APP_IMAGE_URL}${slicer}=w200?authuser=0`;
+  let distance = Math.ceil(distanceKm / 1000);
   return (
     <li className="rec-card">
       <img src={imageUrl} alt={date} className="img-src" />
       <div className="text-container">
         <h1 className="event-name">{eventName}</h1>
-        <p>{distanceKm / 1000}</p>
+        <div className="event-details">
+          <p className="city-name">{cityName}</p>
+          <div className="dis-date-con">
+            <p className="distance">{distance} KM</p>
+            <p className="weather">{weather}</p>
+          </div>
+        </div>
       </div>
     </li>
   );
--- src/components/ScrollEvent/index.js
+++ src/components/ScrollEvent/index.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { ThreeDots } from "react-loader-spinner";
+import "./index.css";
 
 const upcomingApi = process.env.REACT_APP_UPCOMING;
-console.log(upcomingApi);
 
 const ScrollEvent = () => {
   const [data, setData] = useState([]);
@@ -23,7 +24,7 @@ const ScrollEvent = () => {
   const handleScrollEvent = () => {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
     if (data.length <= 44) {
-      if (scrollTop + clientHeight >= scrollHeight) {
+      if (scrollTop + clientHeight >= scrollHeight && loading === false) {
         // console.log(scrollTop, clientHeight, scrollHeight);
         setLoading(true);
         setPage((prev) => prev + 1);
@@ -37,14 +38,37 @@ const ScrollEvent = () => {
   });
 
   return (
-    <div className="app-container">
-      {/* <button onClick={(e) => setAct(event + 1)}>{event}</button> */}
-      {data.map((each) => (
-        <div className="card-scroll" key={uuidv4()}>
-          <p>{each.eventName}</p>
-        </div>
-      ))}
-      {loading && <p>Time to fetch...</p>}
+    <div className="infinite-load-container">
+      <ul className="card-scroll">
+        {data.map((each) => {
+          const { cityName, eventName } = each;
+          console.log(each);
+          return (
+            <li className="load-child-ele" key={uuidv4()}>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyvetnLOz5AF4JPJGxqw0EJpwpBHl9swwqww&s"
+                alt="img"
+              />
+              <div className="load-child-text">
+                <p className="load-city-name">{cityName}</p>
+                <p>{eventName}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {loading && (
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="blue"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      )}
     </div>
   );
 };
